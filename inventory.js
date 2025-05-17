419e5aae1bd1f952be8333bbdbe1ff1cc2004dcb
Nithin K Thomas <64864340+nithinkthomas51@users.noreply.github.com>
2025-05-17 21:57:48 +0100

    Updated the implementation to web components

---
+++ inventory.js
@@ -0,0 +1,18 @@
+const inventoryTemplate = document.createElement('inventoryTemplate');
+inventoryTemplate.innerHTML = `<div class="inventory" id="inventory">
+                               </div>`;
+
+class Inventory extends HTMLElement {
+    constructor() {
+        this.prices = {};
+
+    }
+
+    connectedCallback() {
+        const response = fetch("./products.json");
+        const products = response.json();
+        console.log(products);
+    }
+}
+
+window.customElements.define('inventory', Inventory);
