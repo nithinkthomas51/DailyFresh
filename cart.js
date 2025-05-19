be4499f50bac8663fe6f80d5210655a71c44715a
Nithin K Thomas <64864340+nithinkthomas51@users.noreply.github.com>
2025-05-19 23:33:21 +0100

    Added Cart grid

---
+++ cart.js
@@ -0,0 +1,15 @@
+const cartTemplate = document.createElement("template");
+cartTemplate.innerHTML = `<link rel="stylesheet" href="index.css" />
+                          <h2 class="cart-title" id="cart-title">CART</h2>
+                          <div class="cart" id="cart">
+                          </div>`;
+
+class Cart extends HTMLElement {
+  constructor() {
+    super();
+    this.attachShadow({ mode: "open" });
+    this.shadowRoot.appendChild(cartTemplate.content.cloneNode(true));
+  }
+}
+
+window.customElements.define("cart-component", Cart);
