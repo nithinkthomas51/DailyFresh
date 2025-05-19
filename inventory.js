e6e72067b892943049f5b96cc07b4f2621b48a04
Nithin K Thomas <64864340+nithinkthomas51@users.noreply.github.com>
2025-05-19 23:29:29 +0100

    Changes for adding the inventory grid.

--- inventory.js
+++ inventory.js
@@ -1,35 +1,36 @@
-const inventoryTemplate = document.createElement('template');
-inventoryTemplate.innerHTML = `<div class="inventory" id="inventory">
+const inventoryTemplate = document.createElement("template");
+inventoryTemplate.innerHTML = `<link rel="stylesheet" href="index.css" />
+                               <h2 class="inv-title" id="inv-title">INVENTORY</h2>
+                               <div class="inventory" id="inventory">
                                </div>`;
 
 class Inventory extends HTMLElement {
-    constructor() {
-        super();
-        this.prices = {};
-        this.basket = {};
-        this.attachShadow({mode: 'open'});
-        this.shadowRoot.appendChild(inventoryTemplate.content.cloneNode(true));
-        this.fetchProducts();
-        this.renderProducts();
-    }
+  constructor() {
+    super();
+    this.prices = {};
+    this.basket = {};
+    this.attachShadow({mode: 'open'});
+    this.shadowRoot.appendChild(inventoryTemplate.content.cloneNode(true));
+    this.fetchProducts();
+  }
 
-    async fetchProducts() {
-        const response = await fetch("./products.json");
-        const products = await response.json();
+  async fetchProducts() {
+    const response = await fetch("./products.json");
+    const products = await response.json();
 
-        products.forEach( product => {
-            console.log(`Product: ${product.name}, Price: ${product.price}`);
-            this.prices[product.name] = product.price;
-        });
-    }
+    products.forEach( product => {
+      this.prices[product.name] = product.price;
+    });
+    this.renderProducts();
+  }
 
-    renderProducts() {
-        let productHTML = "";
-        for (const [name, price] of Object.entries(this.prices)) {
-            productHTML += `<item-card name="${name}" price="${price}"></item-card>`;
-        }
-        this.shadowRoot.getElementById('inventory').innerHTML = productHTML;
+  renderProducts() {
+    let productHTML = "";
+    for (const [name, price] of Object.entries(this.prices)) {
+      productHTML += `<item-card name="${name}" price="${price}"></item-card>`;
     }
+    this.shadowRoot.getElementById('inventory').innerHTML = productHTML;
+  }
 }
 
 window.customElements.define('inventory-component', Inventory);
--- itemCard.js
+++ itemCard.js
@@ -1,40 +1,41 @@
 const template = document.createElement('template');
-template.innerHTML = `<div class="card-container">
+template.innerHTML = `<link rel="stylesheet" href="index.css" />
+                      <div class="card-container">
                         <div class="card" id="card">
-                            <h2 class="card-title" id="card-title"></h2>
+                            <h2 class="product-name" id="product-name"></h2>
                             <p id="product-price"></p>
-                            <button class="card=button" id="card-button">Add To Cart</button>
+                            <button class="card-button" id="card-button">Add To Cart</button>
                         </div>
                       </div>`;
 
 class ItemCard extends HTMLElement {
-    constructor() {
-        super();
-        this.addedToCart = false;
-        this.attachShadow({mode: 'open'});
-        this.shadowRoot.appendChild(template.content.cloneNode(true));
-        this.shadowRoot.getElementById('product-name').innerText = this.getAttribute('name');
-        this.shadowRoot.getElementById('product-price').innerText = `€{this.getAttribute('price')}`;
+  constructor() {
+    super();
+    this.addedToCart = false;
+    this.attachShadow({mode: 'open'});
+    this.shadowRoot.appendChild(template.content.cloneNode(true));
+    this.shadowRoot.getElementById('product-name').innerText = this.getAttribute('name').toUpperCase();
+    this.shadowRoot.getElementById('product-price').innerText = `€${this.getAttribute('price')}`;
+  }
+
+  updateCart() {
+    this.addedToCart = !this.addedToCart;
+    let addToCartBtn = this.shadowRoot.getElementById('card-button');
+    if (this.addedToCart) {
+      addToCartBtn.innerText = 'Remove From Cart';
+    } else {
+      addToCartBtn.innerText = 'Add To Cart';
     }
 
-    updateCart() {
-        this.addedToCart = !this.addedToCart;
-        let addToCartBtn = this.shadowRoot.getElementById('add-to-cart');
-        if (this.addedToCart) {
-            addToCartBtn.innerText = 'Remove From Cart';
-        } else {
-            addToCartBtn.innerText = 'Add To Cart';
-        }
+  }
 
-    }
+  connectedCallback() {
+    this.shadowRoot.getElementById('card-button').addEventListener('click', () => this.updateCart());
+  }
 
-    connectedCallback() {
-        this.shadowRoot.getElementById('add-to-cart').addEventListener('click', () => this.updateCart());
-    }
-
-    disconnectedCallback() {
-        this.shadowRoot.getElementById('add-to-cart').removeEventListener();
-    }
+  disconnectedCallback() {
+    this.shadowRoot.getElementById('card-button').removeEventListener();
+  }
 }
 
 window.customElements.define('item-card', ItemCard);
