88766399af19cf19aef529fc2f57a80e854bdd15
Nithin K Thomas <64864340+nithinkthomas51@users.noreply.github.com>
2025-05-20 14:46:10 +0100

    added field for total

--- cart.js
+++ cart.js
@@ -2,7 +2,8 @@ const cartTemplate = document.createElement("template");
 cartTemplate.innerHTML = `<link rel="stylesheet" href="index.css" />
                           <h2 class="cart-title" id="cart-title">CART</h2>
                           <div class="cart" id="cart">
-                          </div>`;
+                          </div>
+                          <h3 id="total-title"></h3>`;
 
 class Cart extends HTMLElement {
   constructor() {
